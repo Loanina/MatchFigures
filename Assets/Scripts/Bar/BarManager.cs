173cd6dc801c115273fcfd61536e6dcfe7773cdf
Loanina <96134664+Loanina@users.noreply.github.com>
2025-05-13 13:20:41 +0300

    Destroy bar figures when match 3

--- Assets/Scripts/Bar/BarManager.cs
+++ Assets/Scripts/Bar/BarManager.cs
@@ -9,22 +9,47 @@ namespace Bar
         [SerializeField] private GameObject barFigurePrefab;
 
         private List<GameObject> currentFigures = new();
+        private Dictionary<string, List<BarFigureView>> figureGroups = new();
         private const int MaxFigures = 7;
+        private const int MatchCount = 3;
 
         public bool TryAddFigure(Sprite shapeSprite, Color backgroundColor, Sprite iconSprite)
         {
             if (currentFigures.Count >= MaxFigures)
                 return false;
 
-            var figure = Instantiate(barFigurePrefab, barParent);
-            figure.GetComponent<BarFigureView>().Setup(shapeSprite, backgroundColor, iconSprite);
-            currentFigures.Add(figure);
+            var obj = Instantiate(barFigurePrefab, barParent);
+            var barFigureView = obj.GetComponent<BarFigureView>();
+                barFigureView.Setup(shapeSprite, backgroundColor, iconSprite);
+            currentFigures.Add(obj);
 
+            var key = GetFigureKey(shapeSprite, iconSprite, backgroundColor);
+
+            if (!figureGroups.ContainsKey(key))
+                figureGroups[key] = new List<BarFigureView>();
+
+            figureGroups[key].Add(barFigureView);
+            
+            if (figureGroups[key].Count == MatchCount)
+            {
+                foreach (var fig in figureGroups[key])
+                {
+                    Destroy(fig.gameObject);
+                    currentFigures.Remove(fig.gameObject);
+                }
+                figureGroups.Remove(key);
+            }
+            
             if (currentFigures.Count == MaxFigures)
                 Debug.Log("Проигрыш! Бар заполнен.");
 
             return true;
         }
 
+        private string GetFigureKey(Sprite shape, Sprite icon, Color color)
+        {
+            var colorKey = $"{color.r:F2}_{color.g:F2}_{color.b:F2}_{color.a:F2}";
+            return $"{shape.name}_{icon.name}_{colorKey}";
+        }
     }
 }
